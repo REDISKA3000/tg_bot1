cb9360d8632611aae3173bf1ba502af76e1e8520
REDISKA3000 <gladilin1908@gmail.com>
2021-12-26 04:03:19 +0300

    Initial commit

--- SQLighter.py
+++ SQLighter.py
@@ -7,10 +7,10 @@ class SQLighter:
         self.connection = sqlite3.connect(database)
         self.cursor = self.connection.cursor()
 
-    def select_all(self):
+    def select_all(self,table):
         """ Получаем все строки """
         with self.connection:
-            return self.cursor.execute('SELECT * FROM mus').fetchall()
+            return self.cursor.execute('SELECT cor, incor FROM {}'.format(table)).fetchall()
 
     def select_single(self, rownum):
         """ Получаем одну строку с номером rownum """
@@ -27,3 +27,14 @@ class SQLighter:
     def close(self):
         """ Закрываем текущее соединение с БД """
         self.connection.close()
+
+    def create(self, user_id):
+        self.cursor.execute('CREATE TABLE IF NOT EXISTS stata(id INTEGER, cor INTEGER, incor INTEGER)')
+        if not(user_id in self.cursor.execute('SELECT id FROM stata').fetchall()[:][0]):
+            print(self.cursor.execute('SELECT id FROM stata').fetchall())
+            self.cursor.execute('INSERT INTO stata (id,cor,incor) VALUES ({},0,0)'.format(user_id))
+            self.cursor.execute('COMMIT')
+
+    def update(self, user_id, c, b):
+        self.cursor.execute('UPDATE stata SET cor = cor+{}, incor= incor + {} WHERE id == {} '.format(b,c,user_id))
+        self.cursor.execute('COMMIT')
--- bot1.py
+++ bot1.py
@@ -6,8 +6,79 @@ from SQLighter import SQLighter
 import utils
 import random
 import sqlite3
+import pandas as pd
+import matplotlib.pyplot as plt
 
 bot = telebot.TeleBot(config.token)
+
+
+commands = ['/start','/test','/game','/stata']
+k = True
+@bot.message_handler(func=lambda message:not(message.text in commands),content_types=['text'])
+def f(message):
+    global k
+    if k:
+        bot.send_message(message.chat.id,'If you you want to start use command /start')
+        k = False
+    else:
+        check_answer(message)
+
+
+def check_answer(message):
+    # Если функция возвращает None -> Человек не в игре
+    answer = utils.get_answer_for_user(message.chat.id)
+    # Как Вы помните, answer может быть либо текст, либо None
+    # Если None:
+    if not answer:
+        bot.send_message(message.chat.id, 'Чтобы начать игру, выберите команду /game')
+    else:
+        # Уберем клавиатуру с вариантами ответа.
+        keyboard_hider = telebot.types.ReplyKeyboardRemove()
+        # Если ответ правильный/неправильный
+        if message.text == answer:
+            bot.send_message(message.chat.id, 'Верно!', reply_markup=keyboard_hider)
+            c=0
+            b=1
+        else:
+            bot.send_message(message.chat.id, 'Увы, Вы не угадали. Попробуйте ещё раз!',
+                             reply_markup=keyboard_hider)
+            c=1
+            b=0
+        db = SQLighter(config.database_name)
+        db.update(user_id, c, b)
+        db.close()
+        bot.send_message(message.chat.id,'If you want to look at your statistics use command /stata')
+        # Удаляем юзера из хранилища (игра закончена)
+        # utils.finish_user_game(message.chat.id)
+
+
+@bot.message_handler(commands=['stata'])
+def stat(message):
+    import matplotlib.pyplot as plt
+    db1=SQLighter(config.database_name)
+    l = list(db1.select_all('stata')[0][:])
+    x = list(map(lambda x: int(x/sum(l)*100),l))
+    print(x)
+    y=['Correct','Incorrect']
+    plt.bar(y,x)
+    plt.savefig('statistics.png')
+    img = open('statistics.PNG', 'rb')
+    bot.send_photo(message.chat.id,photo=img)
+
+
+
+
+
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    global user_id
+    db = SQLighter(config.database_name)
+    user_id = message.from_user.id
+    db.create(user_id)
+    db.close()
+    bot.send_message(message.chat.id,'If you want to test this bot use command /test')
+    bot.send_message(message.chat.id, 'If you want to play a game use command /game')
 
 
 @bot.message_handler(commands=['test'])
@@ -43,24 +114,6 @@ def send_voice(message,rownum):
     msg = bot.send_voice(message.chat.id, p, None)
     return bot.send_message(message.chat.id, msg.voice.file_unique_id, reply_to_message_id=msg.message_id)
 
-@bot.message_handler(func=lambda message: True, content_types=['text'])
-def check_answer(message):
-    # Если функция возвращает None -> Человек не в игре
-    answer = utils.get_answer_for_user(message.chat.id)
-    # Как Вы помните, answer может быть либо текст, либо None
-    # Если None:
-    if not answer:
-        bot.send_message(message.chat.id, 'Чтобы начать игру, выберите команду /game')
-    else:
-        # Уберем клавиатуру с вариантами ответа.
-        keyboard_hider = telebot.types.ReplyKeyboardRemove()
-        # Если ответ правильный/неправильный
-        if message.text == answer:
-            bot.send_message(message.chat.id, 'Верно!', reply_markup=keyboard_hider)
-        else:
-            bot.send_message(message.chat.id, 'Увы, Вы не угадали. Попробуйте ещё раз!', reply_markup=keyboard_hider)
-        # Удаляем юзера из хранилища (игра закончена)
-        #utils.finish_user_game(message.chat.id)
 
 
 if __name__ == '__main__':
